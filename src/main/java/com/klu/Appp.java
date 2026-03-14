c89e4e7d672b9df086265b3a81e6f54fff1ec604
Hasini <2400032318@kluniversity.in>
2026-03-14 17:04:34 +0530

    Skill 4 Spring Experiment

---
+++ src/main/java/com/klu/Appp.java
@@ -0,0 +1,19 @@
+package com.klu;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class Appp {
+
+public static void main(String[] args){
+
+ApplicationContext context =
+new ClassPathXmlApplicationContext("applicationContext.xml");
+
+Student student = (Student) context.getBean("student");
+
+student.display();
+
+}
+
+}
